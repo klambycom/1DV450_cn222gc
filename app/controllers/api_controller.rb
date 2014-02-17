f1c84f7e3f4ad89a61dceb60afb8e575e795abc0
klambycom <christian@klamby.com>
2014-02-17 20:35:20 +0100

    Base class for api controllers

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,28 @@
+class ApiController  < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  protected
+
+    def from_id name
+      begin
+        instance_variable_set "@#{name}", name.camelize.constantize.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render nothing: true, status: 404
+      end
+    end
+
+    def render_result
+      begin
+        yield
+      rescue ActiveRecord::RecordInvalid => e
+        render json: e.record.errors, status: 422
+      rescue
+        render nothing: true, status: 500
+      end
+    end
+
+    def method_missing method, *args, &block
+      return from_id(method.to_s.match(/^find_(.*)$/)[1]) if method.to_s =~ /^find_(.*)$/
+      super
+    end
+end
