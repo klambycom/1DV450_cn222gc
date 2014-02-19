d23cd8b0e770cf0b10c2a8aa93134e0b21cdde68
klambycom <christian@klamby.com>
2014-02-19 15:48:16 +0100

    Highlight current page in developer section

--- app/controllers/developers/base_controller.rb
+++ app/controllers/developers/base_controller.rb
@@ -1,3 +1,14 @@
 class Developers::BaseController < ApplicationController
   layout 'developers'
+
+  protected
+    helper_method :highlight, :highlight?
+
+    def highlight page
+      @highlight_page = page
+    end
+
+    def highlight? page
+      "active" if @highlight_page == page
+    end
 end
