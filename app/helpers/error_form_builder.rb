c66e5741b7f12b1e4900d45b8ec92d50958b22a9
klambycom <christian@klamby.com>
2014-02-19 16:32:22 +0100

    errors_for in register user form

--- app/helpers/error_form_builder.rb
+++
@@ -1,9 +0,0 @@
-class ErrorFormBuilder < ActionView::Helpers::FormBuilder
-  def error_for attribute
-    if @object.errors[attribute].present?
-      @template.content_tag "div",
-                            @object.errors[attribute].join(", "),
-                            class: "text-danger"
-    end
-  end
-end
