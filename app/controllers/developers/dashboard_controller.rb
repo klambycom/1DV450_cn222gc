5c03eb110e222b77d612289e6f52425642044d75
klambycom <christian@klamby.com>
2014-02-19 01:17:41 +0100

    List, show and create applications

--- app/controllers/developers/dashboard_controller.rb
+++ app/controllers/developers/dashboard_controller.rb
@@ -2,12 +2,27 @@ class Developers::DashboardController < Developers::BaseController
   def index
   end
 
-  def show
-  end
-
   def new
+    @application = Doorkeeper::Application.new
   end
 
   def create
+    @application = Doorkeeper::Application.new application_params
+    @application.owner = current_user if Doorkeeper.configuration.confirm_application_owner?
+    if @application.save
+      redirect_to developers_dashboard_url(@application)
+    else
+      render :new
+    end
   end
+
+  def show
+    @application = Doorkeeper::Application.find params[:id]
+  end
+
+  private
+
+    def application_params
+      params.require(:application).permit(:name, :redirect_uri)
+    end
 end
