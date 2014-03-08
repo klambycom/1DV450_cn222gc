4fbb8eaefe729033509ce2d9a0dc727bd1f1ea34
klambycom <christian@klamby.com>
2014-03-08 04:32:07 +0100

    Gravatar directive

--- app/assets/javascripts/controllers/ResourcesController.js
+++ app/assets/javascripts/controllers/ResourcesController.js
@@ -7,6 +7,8 @@ app.controller('ResourcesController', function ($scope, Resource, AlertFactory) 
 		$scope.resources = res.items;
 	}, AlertFactory.error('Resource'));
 
+	$scope.email = 'christian@klamby.com';
+
 	//Resource.query({ id: '94240bb3-b367-4166-bf1b-6cf4dfbb8c66' }, function (res) {
 	//	console.log(res);
 	//}, AlertFactory.error('Resource'));
