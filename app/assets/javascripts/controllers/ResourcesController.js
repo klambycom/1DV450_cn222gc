4ad82c08572927b895a9c7276a77a319faab4842
klambycom <christian@klamby.com>
2014-03-13 00:12:49 +0100

    Tags

--- app/assets/javascripts/controllers/ResourcesController.js
+++ app/assets/javascripts/controllers/ResourcesController.js
@@ -1,15 +1,19 @@
 /*global angular, app, console */
 
 app.controller('ResourcesController', function ($scope, Resource, AlertFactory) {
-	'use strict';
+    'use strict';
 
-	Resource.get(function (res) {
-		$scope.resources = res.items;
-	}, AlertFactory.error('Resource'));
+    var dot = function (a) { return function (b) { return b[a]; }; },
+        flatten = function (a) { return [].concat.apply([], a); };
 
-	$scope.email = 'christian@klamby.com';
+    Resource.get(function (res) {
+        $scope.resources = res.items;
+        $scope.tags = flatten(res.items.map(dot('tags')));
+    }, AlertFactory.error('Resource'));
 
-	//Resource.query({ id: '94240bb3-b367-4166-bf1b-6cf4dfbb8c66' }, function (res) {
-	//	console.log(res);
-	//}, AlertFactory.error('Resource'));
+    $scope.email = 'christian@klamby.com';
+
+    //Resource.query({ id: '94240bb3-b367-4166-bf1b-6cf4dfbb8c66' }, function (res) {
+    //  console.log(res);
+    //}, AlertFactory.error('Resource'));
 });
