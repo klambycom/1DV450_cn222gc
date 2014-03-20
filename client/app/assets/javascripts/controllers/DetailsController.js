c51570270fd584821e5ecc83a8eb0228bef7fe51
klambycom <christian@klamby.com>
2014-03-20 15:19:56 +0100

    Link for removing and editing resource

--- client/app/assets/javascripts/controllers/DetailsController.js
+++ client/app/assets/javascripts/controllers/DetailsController.js
@@ -1,10 +1,20 @@
-/*global app */
+/*global app, angular */
 
-app.controller('DetailsController', ['$scope', '$routeParams', 'Resource', 'Alert',
-    function ($scope, $routeParams, Resource, Alert) {
+app.controller('DetailsController', ['$scope', '$routeParams', 'Resource',
+                                     'Alert', 'User',
+    function ($scope, $routeParams, Resource, Alert, User) {
         'use strict';
+
+        $scope.createdByMe = false;
 
         Resource.get({ id: $routeParams.id }, function (res) {
             $scope.resource = res;
+
+            var currentUser = $scope.$parent.user,
+                resourceUser = $scope.resource.user;
+
+            if (angular.isDefined(currentUser)) {
+                $scope.createdByMe = resourceUser.uuid === currentUser.uuid;
+            }
         }, Alert.error('Resource'));
     }]);
