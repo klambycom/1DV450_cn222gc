211981bcf6e6a79e7b34c763f7e3b45acb020cab
klambycom <christian@klamby.com>
2014-03-20 08:54:37 +0100

    Removed UserController

--- client/app/assets/javascripts/controllers/MainController.js
+++ client/app/assets/javascripts/controllers/MainController.js
@@ -1,23 +1,47 @@
 /*global app, angular */
 
-app.controller('MainController', ['$scope', '$timeout', function ($scope, $timeout) {
-    'use strict';
+app.controller('MainController', ['$scope', '$timeout', 'User', 'Alert',
+    function ($scope, $timeout, User, Alert) {
+        'use strict';
 
-    var alertTimeout;
-    
-    $scope.showAlert = false;
+        // Messages
+        var alertTimeout;
+        
+        $scope.showAlert = false;
 
-    $scope.$on('alert', function (e, msg) {
-        if (angular.isDefined(alertTimeout)) {
-            $timeout.cancel(alertTimeout);
+        $scope.$on('alert', function (e, msg) {
+            if (angular.isDefined(alertTimeout)) {
+                $timeout.cancel(alertTimeout);
+            }
+
+            $scope.message = msg;
+            $scope.showAlert = true;
+
+            alertTimeout = $timeout(function () {
+                $scope.showAlert = false;
+                $scope.message = {};
+            }, 5000);
+        });
+
+        // User
+        $scope.isAuthenticated = User.authenticated;
+
+        $scope.login = User.login;
+
+        $scope.logout = function () {
+            $scope.isAuthenticated = false;
+            User.logout();
+        };
+
+        if (User.authenticated) {
+            User.me(function (res) {
+                $scope.user = {
+                    'uuid': res.uuid,
+                    'name': res.firstname + " " + res.lastname,
+                    'firstname': res.firstname,
+                    'lastname': res.lastname,
+                    'email': res.email
+                };
+            }, Alert.error('User'));
         }
-
-        $scope.message = msg;
-        $scope.showAlert = true;
-
-        alertTimeout = $timeout(function () {
-            $scope.showAlert = false;
-            $scope.message = {};
-        }, 5000);
-    });
-}]);
+    }]);
